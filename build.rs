87b2993f9df79fde807756c0f53749d32776aa05
Jorge Aparicio <jorge@japaric.io>
2018-10-30 22:16:15 +0100

    thumbv6m-none-eabi support

--- build.rs
+++ build.rs
@@ -1,7 +1,13 @@
 use std::{env, fs::File, io::Write, path::PathBuf};
 
 fn main() {
+    let target = env::var("TARGET").unwrap();
+    if target.starts_with("thumbv7m") || target.starts_with("thumbv7em") {
+        println!("cargo:rustc-cfg=armv7m")
+    }
+
     let out = &PathBuf::from(env::var_os("OUT_DIR").unwrap());
+
     File::create(out.join("device.x"))
         .unwrap()
         .write_all(include_bytes!("device.x"))
